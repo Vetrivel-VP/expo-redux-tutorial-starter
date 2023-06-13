54ae8b4aae2ee010f679b6bc9a43b0038f4cc829
Vetrivel Ravi <vetrivel.galaxy@gmail.com>
2023-06-13 19:47:53 +0530

    Changes : Starter Template Created

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center space-y-6">
+      <Text className="text-3xl font-bold text-zinc-600">Counter</Text>
+      <View className="flex items-center justify-center p-12 border border-zinc-400 rounded-xl">
+        <Text className="text-3xl font-bold text-zinc-600">0</Text>
+      </View>
+
+      <View className="flex flex-row items-center justify-evenly w-full">
+        <TouchableOpacity className="py-3 px-5  bg-blue-500  rounded-xl flex items-center justify-center">
+          <Text className="text-3xl font-bold text-gray-50">+</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="py-3 px-5  bg-blue-500 rounded-xl flex items-center justify-center">
+          <Text className="text-3xl font-bold text-gray-50">-</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
